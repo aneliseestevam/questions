55c02ac21646c9e29da52b05426d834eaecb0a98
Anelise Estevam <aestevam.dev@gmail.com>
2022-07-27 15:11:50 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+//using selectors inside the element
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+
+  btn.addEventListener("click", function () {
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+    question.classList.toggle("show-text");
+  });
+});
+
+// traversing the do
+
+// const btns = document.querySelectorAll(".question-btn");
+// const plusIcon = document.querySelector(".plus-icon");
+// const minusIcon = document.querySelector(".minus-icon");
+
+// btns.forEach(function (btn) {
+//   btn.addEventListener("click", function (e) {
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle("show-text");
+//   });
+// });
